7b6372c0faf915fe368eb05895d9dac6c1b51a11
Valery <valery.debris@gmai.com>
2022-02-02 21:43:49 +0100

    avancement de la structure basique 3

--- includes/navigation.php
+++ includes/navigation.php
@@ -28,9 +28,9 @@
 
                 ?>
 
-<!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
 <!--                <li>-->
 <!--                    <a href="#">Services</a>-->
 <!--                </li>-->
