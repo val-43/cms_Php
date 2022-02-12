d1337bc7580d2c13ff56849fb3193a991b69c6f1
Valery <valery.debris@gmai.com>
2022-02-12 18:23:16 +0100

    avancement de la structure basique 8

---
+++ admin/posts.php
@@ -0,0 +1,49 @@
+<?php include "include/admin_header.php"?>
+
+<div id="wrapper">
+
+    <?php include "include/admin_navigation.php"?>
+
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Panneau d'administration
+                        <small>des articles</small>
+                    </h1>
+<?php
+
+    if(isset($_GET['source'])){
+        $source = $_GET['source'];
+    }else{
+        $source = '';
+    }
+
+    switch($source) {
+        case 'add_post';
+            include "include/add_post.php";
+            break;
+        case 'edit_post';
+            include "include/edit_post.php";
+            break;
+        case 'delete_post';
+            include "include/delete_post.php";
+            break;
+        default:
+        include "include/view_all_posts.php";
+    }
+
+
+?>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "include/admin_footer.php"?>
+
